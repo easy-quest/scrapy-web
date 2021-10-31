c325c4873e729b6163edafc310380cc42f72cbe2
easy-quest <easy-quest@mail.ru>
2021-10-31 10:23:34 +0000

    mouse

--- my_xxx/spiders/kubnews.py
+++ my_xxx/spiders/kubnews.py
@@ -1,14 +1,19 @@
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
-from scrapy.contrib.loader.processor import TakeFirst
-from scrapy.contrib.loader import XPathItemLoader
-from scrapy.selector import HtmlXPathSelector
-from kubnews.items import KubnewsItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader.processors import TakeFirst
+# from scrapy.loader import XPathItemLoader
+# from scrapy.selector import HtmlXPathSelector
+# from kubnews.items import KubnewsItem
 
-class KubnewsSpider(scrapy.Spider):
+class KubnewsSpider(CrawlSpider):
     name = 'kubnews'
-    allowed_domains = ['kubnews.ru']
+    allowed_domains = ['www.kubnews.ru']
     start_urls = ['http://kubnews.ru/proisshestviya/']
+
+    # rules = (
+    #     Rule(SgmlLinkExtractor(allow=('act=home_reg', 'act=home_zone')), follow=True),
+    #     Rule(SgmlLinkExtractor(allow=('act=home_more')), callback='parse_item'),
+    # )
 
     def parse(self, response):
         pass
